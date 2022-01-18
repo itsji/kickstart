d6d8d4a8f2d808c6fdb53c1e925c63b6e6bf2850
Ji Shin <dev.jishin@gmail.com>
2022-01-17 22:25:34 -0500

    Replace dotenv configurations with Next.js and refactor

--- config.js
+++
@@ -1,8 +0,0 @@
-const dotenv = require('dotenv');
-dotenv.config({ path: `${__dirname}/.env` });
-
-module.exports = {
-    phrase: process.env.PHRASE,
-    endpoint: process.env.ENDPOINT,
-    address: process.env.ADDRESS
-};
--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,10 +1,9 @@
 import web3 from './web3';
 import CampaignFactory from './build/CampaignFactory.json';
-const { address } = require('../config');
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  address
+  process.env.ADDRESS
 );
 
 export default instance;
